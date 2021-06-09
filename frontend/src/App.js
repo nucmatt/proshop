1e6b7037d1c68324db61145dcb097ac8cf35dc31
Matthew Wessel <nucmatt@gmail.com>
2021-06-09 10:24:11 -0500

    Build: Add Header and Footer components
    Adds the Header and Footer components. ReactBootstrap and ReactIcons are
     brought in here for styling and icons throughout the project.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-
+import { Container } from 'react-bootstrap';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 const App = () => {
-  return (
-    <>
-     <h1>Welcome To ProShop</h1>
-    </>
-  );
-}
+	return (
+		<>
+			<Header />
+			<main className="py-3">
+        <Container>
+				<h1>Welcome To ProShop</h1>
+        </Container>
+			</main>
+			<Footer />
+		</>
+	);
+};
 
 export default App;
