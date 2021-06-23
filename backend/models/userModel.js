67fb53ee0e28140b283dfbde0492dbee352ddb62
Matthew Wessel <nucmatt@gmail.com>
2021-06-23 11:38:38 -0500

    Build: Create user authentication endpoint
    Creates the files and logic necessary to hit a user login authentication
    endpoint. Currently the route is verified with Postman but performs no
    actions. Password is currently sent as plaintext but will be converted
    to JSON Web Tokens later.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
 	{
@@ -25,6 +26,11 @@ const userSchema = mongoose.Schema(
 		timestamps: true,
 	}
 );
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+	// You can use this.password here because this method is called on a specific user object so the 'this' here will refer to that user object, which contains a password property
+	return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema);
 
