1113412eef1420c8168a59c7d2c9d0590a09f53a
Matthew Wessel <nucmatt@gmail.com>
2021-06-30 14:48:53 -0500

    Build: Add updateToPaid order route
    Adds the route and action to update the database isPaid status to paid
    in the backend.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,4 @@
+import { request } from 'express';
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
@@ -56,4 +57,29 @@ const getOrderById = asyncHandler(async (req, res) => {
 	}
 });
 
-export { addOrderItems, getOrderById };
+// @desc Update order to paid
+// @route GET /api/orders/:id/pay
+// @access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id);
+
+	if (order) {
+		order.isPaid = true;
+		order.paidAt = Date.now();
+		order.paymentResult = {
+			id: req.body.id,
+			status: request.body.status,
+			update_time: request.body.update_time,
+			email_address: req.body.payer.email_address,
+		};
+
+		const updatedOrder = await order.save();
+
+		res.json(updatedOrder);
+	} else {
+		res.status(404);
+		throw new Error('Order not found');
+	}
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
