fe77499db718cc64661adc5dfd1952bbf3a201c5
Matthew Wessel <nucmatt@gmail.com>
2021-07-06 15:38:10 -0500

    Build: Add get and update user admin routes
    Adds getUserById and updateUser to the userController to allow for
    admin's to get a user's information and update the user's information.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -30,13 +30,13 @@ const protect = asyncHandler(async (req, res, next) => {
 	}
 });
 
-const isAdmin = (req, res, next) => {
-	if(req.user && req.user.isAdmin) {
+const admin = (req, res, next) => {
+	if (req.user && req.user.isAdmin) {
 		next();
 	} else {
 		res.status(401);
 		throw new Error('Not authorized as an admin');
 	}
-}
+};
 
-export { protect, isAdmin };
+export { protect, admin };
