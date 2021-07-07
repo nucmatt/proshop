d632b13bc1ccabedc437c386277ac55d3dd8b382
Matthew Wessel <nucmatt@gmail.com>
2021-07-07 09:10:55 -0500

    Build: Add UserEditScreen
    Adds the form for editing a user to a new screen, UserEditScreen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import PaymentScreen from './screens/PaymentScreen.js';
 import PlaceOrderScreen from './screens/PlaceOrderScreen.js';
 import OrderScreen from './screens/OrderScreen.js';
 import UserListScreen from './screens/UserListScreen.js';
+import UserEditScreen from './screens/UserEditScreen.js';
 
 const App = () => {
 	return (
@@ -30,6 +31,7 @@ const App = () => {
 					<Route path='/product/:id' component={ProductScreen} />
 					<Route path='/cart/:id?' component={CartScreen} />
 					<Route path='/admin/userlist' component={UserListScreen} />
+					<Route path='/admin/user/:id/edit' component={UserEditScreen} />
 					<Route path='/' component={HomeScreen} exact />
 				</Container>
 			</main>
