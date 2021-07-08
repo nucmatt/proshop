5a7f29a39e1cb95ac3c71c20d8ac3ebf01c38523
Matthew Wessel <nucmatt@gmail.com>
2021-07-08 14:25:44 -0500

    Build: Add ProductEditScreen
    Adds the ProductEditScreen to allow an admin to edit a product via a
    link in the product list. Screen currently does not hit the endpoint to
    update the product in the database.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen.js';
 import UserListScreen from './screens/UserListScreen.js';
 import UserEditScreen from './screens/UserEditScreen.js';
 import ProductListScreen from './screens/ProductListScreen.js';
+import ProductEditScreen from './screens/ProductEditScreen.js';
 
 const App = () => {
 	return (
@@ -34,6 +35,7 @@ const App = () => {
 					<Route path='/admin/userlist' component={UserListScreen} />
 					<Route path='/admin/user/:id/edit' component={UserEditScreen} />
 					<Route path='/admin/productlist' component={ProductListScreen} />
+					<Route path='/admin/product/:id/edit' component={ProductEditScreen} />
 					<Route path='/' component={HomeScreen} exact />
 				</Container>
 			</main>
