339de5edd6689a7189b644536226909541a3f6fe
Matthew Wessel <nucmatt@gmail.com>
2021-07-13 09:46:06 -0500

    Build: Add product review endpoint
    Adds an endpoint for a user to add a review of a product as long as the
    user hasn't yet reviewed that product.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -52,7 +52,7 @@ const createProduct = asyncHandler(async (req, res) => {
 		category: 'Sample category',
 		countInStock: 0,
 		numReviews: 0,
-		description: 'Sample description'
+		description: 'Sample description',
 	});
 
 	const createdProduct = await product.save();
@@ -63,11 +63,12 @@ const createProduct = asyncHandler(async (req, res) => {
 // 	@route PUT /api/products/:id
 // 	@access Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
-	const { name, price, description, image, brand, category, countInStock } = req.body;
+	const { name, price, description, image, brand, category, countInStock } =
+		req.body;
 
 	const product = await Product.findById(req.params.id);
 
-	if(product) {
+	if (product) {
 		product.name = name;
 		product.price = price;
 		product.description = description;
@@ -82,7 +83,54 @@ const updateProduct = asyncHandler(async (req, res) => {
 		res.state(404);
 		throw new Error('Product not found');
 	}
-})
+});
 
+// @desc Create new review
+// @route POST /api/products/:id/reviews
+// @access Private
 
-export { getProducts, getProductById, deleteProduct, createProduct, updateProduct };
+const createProductReview = asyncHandler(async (req, res) => {
+	const { rating, comment } = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		const alreadyReviewed = product.reviews.find(
+			(review) => review.user.toString() === req.user._id.toString()
+		);
+
+		if (alreadyReviewed) {
+			res.status(400);
+			throw new Error('Product already reviewed');
+		}
+
+		const review = {
+			name: req.user.name,
+			rating: Number(rating),
+			comment,
+			user: req.user._id,
+		};
+
+		product.reviews.push(review);
+
+		(product.numReviews = product.reviews.length),
+			(product.rating =
+				product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+				product.reviews.length);
+
+		await product.save();
+		res.status(201).json({ message: 'Review added' });
+	} else {
+		res.status(404);
+		throw new Error('Product not found');
+	}
+});
+
+export {
+	getProducts,
+	getProductById,
+	deleteProduct,
+	createProduct,
+	updateProduct,
+	createProductReview,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,11 +6,18 @@ import {
 	deleteProduct,
 	updateProduct,
 	createProduct,
+	createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct);
+router
+	.route('/:id')
+	.get(getProductById)
+	.delete(protect, admin, deleteProduct)
+	.put(protect, admin, updateProduct);
+
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
