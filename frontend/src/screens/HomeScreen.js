61111b8d3a1dfb8ecf13d8d6a34d003af7833eda
Matthew Wessel <nucmatt@gmail.com>
2021-07-16 09:13:55 -0500

    Build: Add ProductCarousel component
    Adds the ProductCarousel component and brings it into the HomeScreen.
    Custom styling is added to the carousel in index.css.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import Product from '../components/Product.js';
 import Loader from '../components/Loader.js';
 import Message from '../components/Message.js';
 import Paginate from '../components/Paginate.js';
+import ProductCarousel from '../components/ProductCarousel.js';
 import { listProducts } from '../actions/productActions';
 
 const HomeScreen = ({ match }) => {
@@ -23,6 +24,7 @@ const HomeScreen = ({ match }) => {
 
 	return (
 		<>
+			{!keyword && <ProductCarousel />}
 			<h1>Latest Products</h1>
 			{loading ? (
 				<Loader />
