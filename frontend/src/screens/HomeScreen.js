85cce37078f29fbb144d045f2dc6aec990c96320
Matthew Wessel <nucmatt@gmail.com>
2021-07-16 09:40:46 -0500

    Build: Add Go Back button
    Adds a Go Back button to return to the main HomeScreen after a product
    search is executed.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product.js';
@@ -26,7 +27,7 @@ const HomeScreen = ({ match }) => {
 	return (
 		<>
 			<Meta />
-			{!keyword && <ProductCarousel />}
+			{!keyword ? <ProductCarousel /> : <Link to='/' className='btn btn-light'>Go Back</Link>}
 			<h1>Latest Products</h1>
 			{loading ? (
 				<Loader />
